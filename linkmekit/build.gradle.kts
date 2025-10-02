dbbc7017dc23a9acfb8b235d621c45d0c259148f
Tomas Radvansky <tomas@rdev.co.nz>
2025-10-02 21:15:25 +1300

    wip

--- linkmekit/build.gradle.kts
+++ linkmekit/build.gradle.kts
@@ -1,7 +1,13 @@
+import java.util.Properties
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
+
+group = "me.li-nk"
+version = "0.1.0"
 
 android {
     namespace = "me.link.sdk"
@@ -29,6 +35,12 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
@@ -37,4 +49,40 @@ dependencies {
     implementation("com.android.installreferrer:installreferrer:2.2")
     implementation("com.google.android.gms:play-services-ads-identifier:18.0.1")
     testImplementation("junit:junit:4.13.2")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = project.group.toString()
+                artifactId = "linkmekit"
+                version = project.version.toString()
+                pom {
+                    name.set("LinkMeKit")
+                    description.set("Android SDK for LinkMe")
+                    url.set("https://github.com/r-dev-limited/li-nk.me-android-sdk")
+                    licenses {
+                        license {
+                            name.set("Apache-2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("linkme")
+                            name.set("LinkMe")
+                            email.set("support@li-nk.me")
+                        }
+                    }
+                    scm {
+                        url.set("https://github.com/r-dev-limited/li-nk.me-android-sdk")
+                        connection.set("scm:git:https://github.com/r-dev-limited/li-nk.me-android-sdk.git")
+                        developerConnection.set("scm:git:ssh://git@github.com/r-dev-limited/li-nk.me-android-sdk.git")
+                    }
+                }
+            }
+        }
+    }
 }
